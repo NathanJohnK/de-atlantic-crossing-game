d17f8add510e5884e7529265dbdf5fb0dea0d983
NathanJohnK <nathanstuttard@yahoo.co.uk>
2024-09-22 18:47:35 +0100

    Update: added a test to check that the Class can accept a name for the ship. Test runs correctly

--- src/Atlantic_crossing.py
+++ src/Atlantic_crossing.py
@@ -1,8 +1,8 @@
-class Ship:()
-def __init__(self, name, hit_points, armor, speed, position):
-     self.name = name
-     self.hit_points = hit_points
-     self.armour = armor
-     self.speed = speed
-     self.position = position
-     
+class Ship:
+     def __init__(self, name, hit_points, armor, speed, position):
+          self.name = name
+          self.hit_points = hit_points
+          self.armour = armor
+          self.speed = speed
+          self.position = position
+          
--- tests/test_atlantic_crossing.py
+++ tests/test_atlantic_crossing.py
@@ -3,9 +3,11 @@ import pytest
 
 #check that the properties of the class are present
 
-def test_ship_has
-
-#Arrange
-#act
-#Assert
-
+def test_ship_class_has_the_property_name():
+    test_class = Ship("Greyhound", 100, 250, 75, 0-0  )
+    assert test_class.name == "Greyhound"
+    
+  # def test_name_property_is_present(self):
+     #   test_class = Pokemon("Eevee", 55, 75, "surf")
+     #   assert test_class.name == "Eevee"
+     
